78d289725ceff2f2f789aaa438ced021151e54bd
Zahin <alvii147@gmail.com>
2019-10-01 19:01:47 -0400

    Initial Commit

---
+++ SmartWardrobe.py
@@ -0,0 +1,90 @@
+import os
+import tensorflow.keras as tensorflow
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import PIL
+import PIL.ImageOps
+from PIL import Image
+from mlxtend.data import loadlocal_mnist
+
+def imageIndex(n):
+	if n == 0:
+		return "a T-SHIRT/TOP"
+	elif n == 1:
+		return "TROUSERS"
+	elif n == 2:
+		return "a PULLOVER"
+	elif n == 3:
+		return "a DRESS"
+	elif n == 4:
+		return "a COAT"
+	elif n == 5:
+		return "SANDALS"
+	elif n == 6:
+		return "a SHIRT"
+	elif n == 7:
+		return "SNEAKERS"
+	elif n == 8:
+		return "a BAG"
+	elif n == 9:
+		return "ANKLE BOOTS"
+	else:
+		return "an unknown object"
+
+while 1:
+	command = input("\nEnter command> ");
+	command_list = command.split()
+	if command_list[0] == "train":
+		x_train, y_train = loadlocal_mnist(
+		images_path = "E:/Programming/teststuff/Tensor/train-images-idx3-ubyte",
+		labels_path = "E:/Programming/teststuff/Tensor/train-labels-idx1-ubyte")
+		
+		x_train = tf.keras.utils.normalize(x_train, axis = 1)
+		
+		model = tf.keras.models.Sequential()
+		model.add(tf.keras.layers.Flatten())
+		model.add(tf.keras.layers.Dense(784, activation=tf.nn.relu))
+		model.add(tf.keras.layers.Dense(196, activation=tf.nn.relu))
+		model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+		
+		model.compile(optimizer = "sgd", loss = "sparse_categorical_crossentropy", metrics = ["accuracy"])
+		model.fit(x_train, y_train, epochs = 3)
+		
+		model.save("smartwardrobe.model")
+	elif command_list[0] == "test":
+		cnt = 0
+		imgArray = []
+		imgNames = []
+		for filename in os.listdir("E:/Programming/SmartWardrobe/img"):
+			if filename.endswith(".jpg"):
+				img = PIL.Image.open("img/" + filename).convert("L")
+				img = img.resize((28, 28), Image.ANTIALIAS)
+				img = PIL.ImageOps.invert(img)
+				img = np.array(img)
+				img = img.flatten()
+				imgArray.append(img)
+				imgNames.append(filename)
+				cnt+=1
+		
+		imgArray = np.array(imgArray)
+		new_model = tf.keras.models.load_model("smartwardrobe.model")
+		predictions = new_model.predict(imgArray)
+		
+		iter = 0
+		while iter < cnt:
+			print("\n" + str(iter + 1) + "). " + imgNames[iter] + " is an image of " + imageIndex(np.argmax(predictions[iter])))
+			iter+=1
+	elif command_list[0] == "display":
+		if len(command_list) < 2:
+			print("type:    display [FILE NAME]")
+		else:
+			im = Image.open("img/" + command_list[1])
+			im.show()
+	elif command_list[0] == "exit":
+		exit()
+	else:
+		print("\nCOMMANDS")
+		print("\ttrain\t\t\ttrain dataset")
+		print("\ttest\t\t\tpredict images in img folder")
+		print("\tdisplay [FILE NAME]\tdisplay image file")
